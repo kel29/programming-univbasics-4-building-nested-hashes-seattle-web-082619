42d2cda33450016b66940be5a26ea60de5f3cc50
Krista <krista.loven.87@gmail.com>
2019-07-03 02:57:40 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -19,7 +19,7 @@ def monopoly_with_third_tier
 		    pieces: 4,
 		    names: {
 		      reading_railroad: {
-		        
+		        mortgage_value: ""
 		      },
 		      pennsylvania_railroad: {},
 		      b_and_o_railroad: {},
